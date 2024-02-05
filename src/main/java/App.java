6e87ee28da2217ac710e147ac4b2440813ede078
akasumv <akasum99@gmail.com>
2024-02-06 01:29:35 +0400

    решение (1)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,8 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Objects;
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
@@ -8,5 +10,10 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Для пары объектов HelloWorld: " + Objects.equals(bean, bean2));
+        System.out.println("Для пары объектов Cat: " + Objects.equals(bean3, bean4));
     }
 }
